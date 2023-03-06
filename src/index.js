5c6882c137359799211f2eb239ef0b2e0593bfd5
dang3rzone <andresrosalespinzon@gmail.com>
2023-03-06 16:55:58 +0100

    feat: eslint dependencies

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import { createRoot } from 'react-dom/client';
+// import { createRoot } from 'react-dom/client'
 
 import App from './components/App';
 
-// const container = document.getElementById('app');
+// const container = document.getElementById('app')
 const root = ReactDOM.createRoot(document.getElementById('root')); // createRoot(container!) if you use TypeScript
 root.render(<App tab="home" />);
