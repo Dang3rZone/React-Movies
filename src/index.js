627ada373921ac60414e02108dc36ed1e675b281
dang3rzone <andresrosalespinzon@gmail.com>
2023-03-06 16:28:14 +0100

    feat: moved app to folder and added eslint

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import { createRoot } from 'react-dom/client';
 
-import App from './App';
+import App from './components/App';
 
 // const container = document.getElementById('app');
 const root = ReactDOM.createRoot(document.getElementById('root')); // createRoot(container!) if you use TypeScript
