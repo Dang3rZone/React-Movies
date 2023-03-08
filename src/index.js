b43ea3d8af1d34b4344fb818e710b9c69a63858a
dang3rzone <andresrosalespinzon@gmail.com>
2023-03-08 16:15:44 +0100

    feat: added mui themming

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 // import { createRoot } from 'react-dom/client'
 
 import App from './components/App';
 
+const theme = createTheme({});
 // const container = document.getElementById('app')
 const root = ReactDOM.createRoot(document.getElementById('root')); // createRoot(container!) if you use TypeScript
 root.render(
-  <BrowserRouter>
-    <App tab="home" />
-  </BrowserRouter>,
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <App tab="home" />
+    </BrowserRouter>
+  </ThemeProvider>
 );
